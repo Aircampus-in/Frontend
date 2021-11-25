d1e23b7bde8a6a408a9c32496d108a42626282d5
Souvik Deb <souvikdeb26@gmail.com>
2021-11-25 16:23:31 +0530

    add toggle feature

--- React-Basics/hello-world/src/components/Counter.js
+++ React-Basics/hello-world/src/components/Counter.js
@@ -4,14 +4,22 @@ import React,{useState} from 'react'
 
 const Counter = ()=>{
     const [counter, setCounter] = useState(0);
+    const [toggle, setToggle] = useState(true);
 
     function incrementer(){
         setCounter(counter + 1);
     }
+
+    function dissapears(){
+        setToggle(!toggle);
+    }
     return(
         <>
+        {toggle? <h6>It's visible</h6> : ''}
+        
         <div>{counter}</div>
         <button onClick={incrementer}>Click</button>
+        <button onClick={dissapears}>Toggle</button>
         </>
     )
 }
