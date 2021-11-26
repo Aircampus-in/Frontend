a95f5d3edd9653f6af87d7b8dd46409d844af737
Souvik Deb <souvikdeb26@gmail.com>
2021-11-26 17:56:27 +0530

    add uuid package

--- React-Basics/hello-world/src/components/Tweets.js
+++ React-Basics/hello-world/src/components/Tweets.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import Tweet from './Tweet';
+import { v4 as uuidv4 } from 'uuid';
 import '../App.css'
 
 const Tweets=()=>{
     const tweets=[
-        {name: "Souvik", tweet: "Hello everyone!!"},
-        {name: "Prakash", tweet: "Hey there!!"},
-        {name: "Vaibhav", tweet: "Hii Bangalore!!"}
+        {name: "Souvik", tweet: "Hello everyone!!", id=uuidv4()},
+        {name: "Prakash", tweet: "Hey there!!", id=uuidv4()},
+        {name: "Vaibhav", tweet: "Hii Bangalore!!", id=uuidv4()}
     ]
 
     return(
         <section className="tweets">
-            {tweets.map((tweet, index)=> (
-            <Tweet key={index} name={tweet.name} tweet={tweet.tweet}/>
+            {tweets.map((tweet)=> (
+            <Tweet key={id} name={tweet.name} tweet={tweet.tweet}/>
             ))}
         </section>
     );
