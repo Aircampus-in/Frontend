99a81211dceca33dac6502f4ffdc4abb9cfee655
Souvik Deb <souvikdeb26@gmail.com>
2021-11-29 10:32:47 +0530

    update counter

--- React-Basics/hello-world/src/components/Counter.js
+++ React-Basics/hello-world/src/components/Counter.js
@@ -11,6 +11,14 @@ const Counter = ()=>{
         setCounter(counter + 1);
     }
 
+    function decrementer(){
+        setCounter(counter - 1);
+    }
+
+    function reset(){
+        setCounter(0);
+    }
+
     function dissapears(){
         setToggle(!toggle);
     }
@@ -19,7 +27,9 @@ const Counter = ()=>{
         <h6 className={toggle? 'inactive' : 'appear' }> Its visible </h6>
         
         <div className="counter">{counter}</div>
-        <button className="counter-btn" onClick={incrementer}>Click</button>
+        <button className="counter-btn" onClick={incrementer}>Plus</button>
+        <button className="counter-btn" onClick={decrementer}>Minus</button>
+        <button className="counter-btn" onClick={reset}>Reset</button>
         <button className="counter-btn" onClick={dissapears}>Toggle</button>
         </>
     )
