b48dbf02133028d68eead13ca145b4dc45b5e494
Souvik Deb <souvikdeb26@gmail.com>
2022-01-08 21:11:25 +0530

    add state

--- React-Basics/React Components/app.js
+++ React-Basics/React Components/app.js
@@ -25,7 +25,7 @@ function Article(props){
                 <h2 className="article-title">{props.title}</h2>
                 <p className="article-para">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto, quibusdam voluptates. Placeat atque eos laudantium eum maxime ea quis, qui nihil ipsum nisi impedit excepturi ullam debitis quam est? Eligendi...</p>
                 <button className="article-btn">Read More</button>
-                <Reaction/>
+                <Reaction />
             
         </div>
     );
@@ -34,33 +34,36 @@ function Article(props){
 
 class Reaction extends React.Component{
 
-
-    state = {
-        like: 0,
-        comment: 0
-    };
-
-    incrementLike() {
-        this.setState({
-            like : this.state.like + 1
-        });
+    constructor(){
+        super()
+        this.state ={
+            likes : 0,
+            comments : 0
+        }
     }
+    
+    
 
     render(){
         return(
-        <div className="reaction-container">
-            <a className="like" onClick={this.incrementLike.bind(this)}>
-               <i className="fas fa-heart fa-2x"></i>
-            </a>
-            <span className="like-counter">{this.state.like}</span>
-            <a className="comment" href="">
-                <i className="fas fa-comment fa-2x"></i>
-            </a>
-            <span className="comment-counter">{this.state.comment}</span>
-        </div>
+            <div className="reaction-container">
+                <a className="like" onClick={this.state.handleClick.bind(this)}>
+                    {
+                    this.state.likes === 0 ? 
+                    <i className="far fa-heart fa-2x"></i>
+                     : 
+                    <i className="fas fa-heart fa-2x"></i>
+                    }
+                </a>
+                <span className="like-counter">{this.state.likes}</span>
+                <a className="comment" href="">
+                    <i className="fas fa-comment fa-2x"></i>
+                </a>
+                <span className="comment-counter">{this.state.comments}</span>
+            </div>
     );
     }
-   
+        
 }
 
 
@@ -88,10 +91,10 @@ function App(){
     return(
         <div>
             <Header/>
-            <Article title= "The Mountains are Calling" img="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"/>
-            <Article title= "The Setting Sun" img="https://images.unsplash.com/photo-1495616811223-4d98c6e9c869?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"/>
-            <Article title= "Of Wooden Cottages and Snowy Mountains" img="https://images.unsplash.com/photo-1520984032042-162d526883e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"/>
-            <Article title= "Food is Happiness" img="https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"/>
+            <Article title= "The Mountains are Calling" img="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+            <Article title= "The Setting Sun" img="https://images.unsplash.com/photo-1495616811223-4d98c6e9c869?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80" />
+            <Article title= "Of Wooden Cottages and Snowy Mountains" img="https://images.unsplash.com/photo-1520984032042-162d526883e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+            <Article title= "Food is Happiness" img="https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
             
             <Footer/>
         </div>
