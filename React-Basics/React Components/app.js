32dd6318cc421b56c7c42e4d04913504883a31a9
Souvik Deb <souvikdeb26@gmail.com>
2022-01-10 15:50:34 +0530

    add pre-defined id properties as keys

--- React-Basics/React Components/app.js
+++ React-Basics/React Components/app.js
@@ -1,14 +1,18 @@
 const articles = [
     { title: "The Mountains are Calling",
-    img:"https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" },
+    img:"https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    id:1 },
 
     {title: "The Setting Sun",
-    img:"https://images.unsplash.com/photo-1495616811223-4d98c6e9c869?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"},
+    img:"https://images.unsplash.com/photo-1495616811223-4d98c6e9c869?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+    id:2},
 
     { title: "Of Wooden Cottages and Snowy Mountains",
-    img:"https://images.unsplash.com/photo-1520984032042-162d526883e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"},
+    img:"https://images.unsplash.com/photo-1520984032042-162d526883e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    id:3},
     
-    {title: "Food is Happiness", img:"https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"}
+    {title: "Food is Happiness", img:"https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    id:4}
 ]
 
 function Header(){
@@ -103,11 +107,11 @@ function Footer(){
 
 
 function App(props){
-    console.log(props.initialArticles)
+    console.log(props.id)
     return(
         <div>
             <Header />
-            {props.initialArticles.map((article)=><Article title={article.name} img={article.img}/>)}
+            {props.initialArticles.map((article)=><Article title={article.name} img={article.img} key={article.id}/>)}
             <Footer/>
         </div>
     );
