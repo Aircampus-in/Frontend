adca072e29a9ea2290f2ad84031ebf29b12f80ea
Souvik Deb <souvikdeb26@gmail.com>
2022-01-19 20:17:09 +0530

    destructure commentForm props

--- React-Basics/news-app/src/components/CommentForm.js
+++ React-Basics/news-app/src/components/CommentForm.js
@@ -3,13 +3,15 @@ import React from 'react';
 class CommentForm extends React.Component{
 
     render(){
+
+        const{comments, handleChange,handleSubmit, value} = this.props;
         return(
             <div>
                 <div>
-                    {this.props.comments.map((c)=><p key={c.id.toString()}>{c.comment}</p>)}
+                    {comments.map((c)=><p key={c.id.toString()}>{c.comment}</p>)}
                 </div>
-                <form className="form-container" onSubmit={this.props.handleSubmit}> 
-                <input type="text" value={this.props.value} placeholder="Enter Comment"  className="comment-box" onChange={this.props.handleChange}/>
+                <form className="form-container" onSubmit={handleSubmit}> 
+                <input type="text" value={value} placeholder="Enter Comment"  className="comment-box" onChange={handleChange}/>
                 <button className="comment-btn"><i className="fas fa-arrow-right fa-2x"></i></button>
                 </form>
             </div>
