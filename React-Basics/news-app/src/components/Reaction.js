5954ea486ee32b4b5874d69b8c5bc510b80ecc79
Souvik Deb <souvikdeb26@gmail.com>
2022-01-19 20:15:47 +0530

    destructure reaction props

--- React-Basics/news-app/src/components/Reaction.js
+++ React-Basics/news-app/src/components/Reaction.js
@@ -4,22 +4,22 @@ import React from 'react';
 class Reaction extends React.Component{
 
     render(){
-        // console.log(this.props.incrementLike)
+        const {commentCounter, incrementLike, likeCounter} = this.props;
         return(
             <div className="reaction-container">
-                <a className="like" href="/" onClick={this.props.incrementLike}>
+                <a className="like" href="/" onClick={incrementLike}>
                     {
-                    this.props.likeCounter === 0 ? 
+                    likeCounter === 0 ? 
                     <i className="far fa-heart fa-2x"></i>
                      : 
                     <i className="fas fa-heart fa-2x"></i>
                     }
                 </a>
-                <span className="like-counter">{this.props.likeCounter}</span>
+                <span className="like-counter">{likeCounter}</span>
                 <a className="comment" href="/">
                     <i className="fas fa-comment fa-2x"></i>
                 </a>
-                <span className="comment-counter">{this.props.commentCounter}</span>
+                <span className="comment-counter">{commentCounter}</span>
             </div>
     );
     }
