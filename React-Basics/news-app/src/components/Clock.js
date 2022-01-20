672aa797134150e3812844de1e31c87f896633d6
Souvik Deb <souvikdeb26@gmail.com>
2022-01-20 20:31:48 +0530

    add clock component

---
+++ React-Basics/news-app/src/components/Clock.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+
+class Clock extends Component{
+
+    state={
+        date : new Date()
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(()=>
+        this.setState({date: new Date()}),1000);
+    }
+
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+
+    render(){
+        return(
+            <div className="timer">{this.state.date.toLocaleTimeString()}</div>
+        )
+    }
+}
+
+
+export default Clock;
--- React-Basics/news-app/src/components/Footer.js
+++ React-Basics/news-app/src/components/Footer.js
@@ -1,13 +1,15 @@
+import Clock from './Clock';
+
 function Footer(){
     return(
         <footer>
-            <div className="container">
+            <div className="container footer-flex">
                 <ul className="social">
                     <li className="social-item"><a href="/">Twitter</a></li>
                     <li className="social-item"><a href="/">LinkedIn</a></li>
                     <li className="social-item"><a href="/">Instagram</a></li>
                 </ul>
-
+                <Clock/>
             </div>
             
         </footer>
