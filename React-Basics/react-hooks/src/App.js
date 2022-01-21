8a9d3772c39a8bde2fc324cd6ba5b31e416bc130
Souvik Deb <souvikdeb26@gmail.com>
2022-01-22 01:21:43 +0530

    update app

--- React-Basics/react-hooks/src/App.js
+++ React-Basics/react-hooks/src/App.js
@@ -1,24 +1,20 @@
 import './App.css';
 import React from 'react';
 
-class App extends React.Component{
+const App=()=>{
+
+  const[count, setCount] = React.useState(0)
 
 
-  state={
-    count: 0
+  const incrementer=()=>{
+    setCount(count+1)
   }
-
-  incrementer=()=>{
-    this.setState({count: this.state.count + 1})
-  }
-  render(){
     return (
       <div className="container">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.incrementer}>Click Me</button>
+        <h1>{count}</h1>
+        <button onClick={incrementer}>Click Me</button>
       </div>
     );
-  }
   
 }
 
