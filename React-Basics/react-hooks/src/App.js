719604f27e559b170d30705f34aeb5da09ba9c50
Souvik Deb <souvikdeb26@gmail.com>
2022-01-22 14:24:58 +0530

    update useEffect

--- React-Basics/react-hooks/src/App.js
+++ React-Basics/react-hooks/src/App.js
@@ -4,13 +4,13 @@ import {useState, useEffect} from 'react';
 const Counter=()=> {
 
   const[count, setCount]= useState(0)
-  const[isVisible, setIsVisible] = useState(true)
+  const[toggle, setToggle] = useState(true)
 
   useEffect(()=>{
     console.log('Use Effect')
     if(count>0){
     document.title=`New Notification (${count})`}
-  },[count])
+  },[])
 
   
 console.log('counter')
@@ -18,11 +18,11 @@ console.log('counter')
   return (
     <div className='container'>
       <p>You clicked {count} times</p>
-      {isVisible? <p>Helloo!!</p> : ''}
+      {toggle? <p>I am visible!!</p> : ''}
       <button onClick={() => setCount(count+1)}>
         Click me
       </button>
-      <button onClick={() => setIsVisible(!isVisible)}>Toggle</button>
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
 
     </div>
   );
