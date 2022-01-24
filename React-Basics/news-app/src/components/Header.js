59a6aa1f6fdc38354faeebb3ecc4ea523d7b68ad
Souvik Deb <souvikdeb26@gmail.com>
2022-01-25 02:50:34 +0530

    add dark mode

--- React-Basics/news-app/src/components/Header.js
+++ React-Basics/news-app/src/components/Header.js
@@ -1,14 +1,18 @@
-
+import { DarkModeContext } from "../context/DarkModeContext";
+import {useContext} from 'react';
 function Header(props){
+
+    const{darkMode, toggleDarkMode} = useContext(DarkModeContext)
     return(
-        <header>
+        <header className={darkMode ? "header, header-dark" : "header header-light"}>
         <div className="container">
             <nav className="navbar">
-                <h1><a href="/">News App</a></h1>
-                <ul className="nav-links">
+                <h1><a className={darkMode ? "brand brand-dark": "brand brand-light"} href="/">News App</a></h1>
+                <ul className={darkMode ? "nav-links nav-links-dark" : "nav-links nav-links-light"}>
                     <li className="nav-item"><a href="/">Heading</a></li>
                     <li className="nav-item"><a href="/">About</a></li>
                     <li className="nav-item"><a href="/">Contact</a></li>
+                    <button className={darkMode ? "toggle-theme dark" : "toggle-theme light"} onClick={toggleDarkMode}>{darkMode ? <i className="fas fa-moon fa-2x"></i> : <i class="fas fa-sun fa-2x"></i>}</button>
                 </ul>
             </nav>
         </div>
