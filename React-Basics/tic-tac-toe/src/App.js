4141cb331e739f1c26d5dca10482b09d75f397c2
Souvik Deb <souvikdeb26@gmail.com>
2022-03-17 22:26:35 +0530

    add useDispatch and useSelector

--- React-Basics/tic-tac-toe/src/App.js
+++ React-Basics/tic-tac-toe/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-// import {useDispatch} from 'react-redux';
-
+import {useDispatch, useSelector} from 'react-redux';
+import {increment, decrement} from './features/counter/counterSlice'
 
 function App() {
+  const dispatch = useDispatch()
+  const count = useSelector((state)=>state.counter.value)
+
+  // const increment=()=>{
+  //   dispatch(increment())
+  // }
+
+  // const decrement=()=>{
+  //   dispatch(decrement())
+  // }
   return (
     <>
-    {/* <button onClick={increment}>+</button> */}
-    {/* <p>{count}</p> */}
-    {/* <button onClick={decrement}>-</button> */}
+    <button onClick={()=>dispatch(increment())}>+</button>
+    <p>{count}</p>
+    <button onClick={()=>dispatch(decrement())}>-</button>
     </>
   );
 }
